0f83ed40308f2000bf61af3b073cf23d55dc1dac
Badim4k-nya <filonovadim11education@gmail.com>
2024-10-01 14:55:23 +0500

    Revert "Угадай рандомно число"
    This reverts commit 8668b8969967452fb4a71a09045556397d6c7f9c.

--- NyaProject/main.cpp
+++ NyaProject/main.cpp
@@ -8,30 +8,18 @@ int main() {
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
 
-	const int size = 10;
-	int arr1[size]{}, arr2[size]{}, couner = 0;
+	int arr[20]{};
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < 20; i++)
 	{
-		arr1[i] = rand() % 6 + 5;
-		std::cout << "¬вдите число от 0 до 10: ";
-		std::cin >> arr2[i];
-		if (arr2[i] >= 0 && arr2[i] <= 10) 
+		arr[i] = rand() % 6 + 5;
+		std::cout << arr[i] << "\t";
+		if (i == 9)
 		{
-			if (arr1[i] == arr2[i])
-			{
-				std::cout << "”дачка:)\n";
-				couner++;
-			} 
-			else 
-			{
-				std::cout << "Ќе удачка:(\n";
-			}
-		}
-		else 
-		{
-			std::cout << "ЁЁЁ! нельз€ так!!!\n\n";
+			std::cout << '\n';
 		}
 	}
-	std::cout << "\t—чЄт: " << couner;
+
+	
+
 }
